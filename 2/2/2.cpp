9618071bb981f0e0222f7df2458873eb22e02995
CaptainAnastasia <92058856+CaptainAnastasia@users.noreply.github.com>
2021-12-02 15:20:50 +0300

    Add files via upload

---
+++ 2/2/2.cpp
@@ -0,0 +1,27 @@
+﻿#include <iostream>
+#include <string>
+#include "Fraction.h"
+
+
+
+int main()
+{
+	Fraction a(1, 2);
+	Fraction b(2, 3);
+	Fraction c = a + b;
+	Fraction d = a - b;
+	Fraction e = a * b;
+	Fraction f = a / b;
+	a.printFraction();
+	b.printFraction();
+	c.printFraction();
+	d.printFraction();
+	e.printFraction();
+	f.printFraction();
+	std::cout << "\n";
+	char test1[] = "0.25";
+	double test2 = 0.25;
+	Fraction::printAsFraction(test1);
+	Fraction::printAsFraction(test2);
+	std::cout << "Total fractions: " << a.getTotalFractions() << "\n";
+}
---
+++ 2/2/Fraction.h
@@ -0,0 +1,29 @@
+﻿#pragma once
+#include <iostream>
+#include <string>
+class Fraction
+{
+private:
+	int n_;
+	int m_;
+	static int fractionCounter;
+public:
+	Fraction();
+	Fraction(int n, int m); // x = n/m
+	void setM(int m);
+	void setN(int n);
+	int getM();
+	int getN();
+	int getTotalFractions();
+
+	Fraction operator +(const Fraction& term);
+	Fraction operator -(const Fraction& subtrahend);
+	Fraction operator *(const Fraction& factor);
+	Fraction operator /(const Fraction& divisor);
+
+	void reduce(); // ñîêðàòèòü
+	static int gcd(int n, int m); // íàèáîëüøèé îáùèé äåëèòåëü ïî àëãîðèòìó åâêëèäà
+	static void printAsFraction(double decimal_fraction); // âûâîä åñëè èíïóò öèôðû
+	static void printAsFraction(char* decimal_fraction); // âûâîä åñëè èíïóò òåêñò
+	void printFraction();
+};
