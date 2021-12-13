87a7ad0223c6a0d3c1763a9bfcd56457133f1adf
CaptainAnastasia <92058856+CaptainAnastasia@users.noreply.github.com>
2021-12-13 21:05:51 +0300

    Add files via upload

---
+++ 3/2/Airliner.h
@@ -0,0 +1,17 @@
+#pragma once
+
+#include "Airplane.h";
+#include "ILoadable.h";
+
+class Airliner : public ILoadable, public Airplane
+{
+public:
+	Airliner();
+	Airliner(string _model, string _airlines, size_t _year_of_manufacture, int _capacity, double _fullness, double _percent_avarage, int _weight, double _cargocapacity);
+
+	void load(int _kilograms) const override;
+};
+
+
+
+
